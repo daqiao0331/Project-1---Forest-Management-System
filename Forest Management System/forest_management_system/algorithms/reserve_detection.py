f2a326719c8d2bcc5a14f6efd689b6110db7df01
daqiao0331 <daqiao0331@yeah.net>
2025-06-26 14:35:11 +0800

    code🌳

--- Forest Management System/forest_management_system/algorithms/reserve_detection.py
+++ Forest Management System/forest_management_system/algorithms/reserve_detection.py
@@ -3,27 +3,23 @@ from forest_management_system.components.health_status import HealthStatus
 DFS algorithm to find reserves in a forest graph
 '''
 def find_reserves(forest_graph):
-    reserves = []  # 存放所有保护区
-    visited = set()  # 记录已经访问过的树
+    reserves = []  # 所有保护区
+    visited = set()  # 已经看过的树
 
-    def dfs(tree_id, reserve):
-        # 如果已经访问过，直接返回
+    def dfs(tree_id, group):
         if tree_id in visited:
             return
-        # 只处理健康的树
         if forest_graph.trees[tree_id].health_status != HealthStatus.HEALTHY:
             return
         visited.add(tree_id)
-        reserve.add(tree_id)
-        # 遍历所有邻居
+        group.add(tree_id)
         for neighbor in forest_graph.get_neighbors(tree_id):
-            dfs(neighbor, reserve)
+            dfs(neighbor, group)
 
-    # 遍历所有树，找到每个健康且未访问的树，作为新保护区的起点
     for tree_id in forest_graph.trees:
         if forest_graph.trees[tree_id].health_status == HealthStatus.HEALTHY and tree_id not in visited:
-            reserve = set()
-            dfs(tree_id, reserve)
-            if reserve:
-                reserves.append(reserve)
+            group = set()
+            dfs(tree_id, group)
+            if group:
+                reserves.append(group)
     return reserves
