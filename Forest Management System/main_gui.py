999516b764f36011698dcf204ba74f1fd8b02984
daqiao0331 <daqiao0331@yeah.net>
2025-06-30 00:25:22 +0800

    code🌳

--- Forest Management System/main_gui.py
+++ Forest Management System/main_gui.py
@@ -11,7 +11,6 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 from forest_management_system.gui.app import AppLogic
 
 def main():
-    """Initializes and runs the application."""
     root = tk.Tk()
     app = AppLogic(root)
     app.run()
